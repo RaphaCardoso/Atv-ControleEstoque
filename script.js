408d574f562adb7c2d9b480b297b8f572f4507b5
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-03-07 14:21:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 // getElementById, addEventListener, objetos
 
 // addEvent
+
+
+
+const exampleInputCod = document.getElementById("InputCod");
+
+const exampleInputProd = document.getElementById("InputProd");
+
+const exampleInputFab = document.getElementById("InputFab");
+
+const exampleInputMod = document.getElementById("InputMod");
+
+const exampleInputQnt = document.getElementById("InputQnt");
+
+const exampleInputValue = document.getElementById("InputValue");
+
+const btnInput = document.getElementById("btnInput");
+
+const data = [];
+
+
+btnInput.addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    const produto = {
+        Nome: exampleInputProd.value,
+        Cod: exampleInputCod.value,
+        Modelo: exampleInputMod.value,
+        Quantidade: exampleInputQnt.value,
+        Valor: exampleInputValue.value,
+    }
+
+    data.push(produto);
+    console.log(data);
+});
